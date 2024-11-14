7966e59fd64ab667fe7deebf57e64ff827c83f38
unknown <k25650727@gmail.com>
2024-11-14 10:45:08 +0100

    commit

--- CsAlgoPractice/Leetcode/L52_NQueensII/NQueensII.cs
+++ CsAlgoPractice/Leetcode/L52_NQueensII/NQueensII.cs
@@ -3,38 +3,29 @@ namespace Leetcode;
 public class NQueensII
 {
 
-    bool[] cols = [];
-    bool[] diag1 = [];
-    bool[] diag2 = [];
-    int n = 0;
     public int TotalNQueens(int n)
     {
-        this.n = n;
-        cols = new bool[n];
-        diag1 = new bool[2 * n - 1];
-        diag2 = new bool[2 * n - 1];
+        bool[] cols = new bool[n];
+        bool[] diag1 = new bool[2 * n - 1];
+        bool[] diag2 = new bool[2 * n - 1];
 
-        return countQueens(0);
-    }
-
-    private int countQueens(int i)
-    {
-        if (i == n)
+        int helper(int i)
         {
-            return 1;
-        }
-        int count = 0;
-        for (int j = 0; j < n; j++)
-        {
-            if (cols[j] || diag1[i + j] || diag2[j - i + n - 1])
+            if (i == n)
             {
-                continue;
+                return 1;
             }
-            cols[j] = diag1[i + j] = diag2[j - i + n - 1] = true;
-            count += countQueens(i + 1);
-            cols[j] = diag1[i + j] = diag2[j - i + n - 1] = false;
+            int count = 0;
+            for (int j = 0; j < n; j++)
+            {
+                if (cols[j] || diag1[i + j] || diag2[j - i + n - 1])
+                    continue;
+                cols[j] = diag1[i + j] = diag2[j - i + n - 1] = true;
+                count += helper(i + 1);
+                cols[j] = diag1[i + j] = diag2[j - i + n - 1] = false;
+            }
+            return count;
         }
-        return count;
+        return helper(0);
     }
-
 }
