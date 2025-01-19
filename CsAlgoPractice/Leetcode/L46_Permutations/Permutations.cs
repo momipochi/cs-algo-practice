728e701750c38b729476a01d11b0a19485ab14a8
unknown <k25650727@gmail.com>
2025-01-19 14:19:39 +0100

    s

---
+++ CsAlgoPractice/Leetcode/L46_Permutations/Permutations.cs
@@ -0,0 +1,33 @@
+﻿namespace CsAlgoPractice.Leetcode.L46_Permutations;
+
+public class Permutations
+{
+    IList<IList<int>> res = new List<IList<int>>();
+    private int[] nums;
+    public IList<IList<int>> Permute(int[] nums)
+    {
+        this.nums = nums;
+        dfs(new List<int>());
+        return res;
+    }
+
+    private void dfs(IList<int> perm)
+    {
+        if (perm.Count == nums.Length)
+        {
+            res.Add(new List<int>(perm));
+            return;
+        }
+        
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (perm.Contains(nums[i]))
+            {
+                continue;
+            }
+            perm.Add(nums[i]);
+            dfs(perm);
+            perm.RemoveAt(perm.Count - 1);
+        }
+    }
+}
---
+++ CsAlgoPractice/Leetcode/L49_GroupAnagrams/GroupAnagram.cs
@@ -0,0 +1,26 @@
+﻿namespace CsAlgoPractice.Leetcode.L49_GroupAnagrams;
+
+public class GroupAnagram
+{
+    public IList<IList<string>> GroupAnagrams(string[] strs)
+    {
+        Dictionary<string,IList<string>> res = new Dictionary<string,IList<string>>();
+
+        foreach (var str in strs)
+        {
+            char[] tmp = str.ToCharArray();
+            Array.Sort(tmp);
+            var stmp = new string(tmp);
+            if (res.ContainsKey(stmp))
+            {
+                res[stmp].Add(str);
+            }
+            else
+            {
+                res[stmp] = new List<string> { str };
+            }
+        }
+        
+        return res.Select(x=>x.Value).ToList();
+    }
+}
