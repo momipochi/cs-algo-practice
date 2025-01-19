45a4eac96c743b22d5564c139e70fdd3754fede7
unknown <k25650727@gmail.com>
2025-01-19 15:18:12 +0100

    s

---
+++ CsAlgoPractice/Leetcode/L56_MergeIntervals/MergeIntervals.cs
@@ -0,0 +1,26 @@
+﻿namespace CsAlgoPractice.Leetcode.L56_MergeIntervals;
+
+public class MergeIntervals
+{
+    public int[][] Merge(int[][] intervals)
+    {
+        Array.Sort(intervals, (a,b)=>a[0]-b[0]);
+        IList<int[]> res = new List<int[]>();
+        int[] prev = intervals[0];
+        for (int i = 0; i < intervals.Length; i++)
+        {
+            if (prev[1] < intervals[i][0])
+            {
+                res.Add(prev);
+                prev = intervals[i];
+            }
+            else
+            {
+                prev[1] = prev[1] > intervals[i][1] ? prev[1] : intervals[i][1];
+            }
+        }
+        res.Add(prev);
+
+        return res.ToArray();
+    }
+}
