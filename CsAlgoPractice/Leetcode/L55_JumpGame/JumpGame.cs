b87a6f7dbc970b2aa1a1d1930d66feac89359d0b
unknown <k25650727@gmail.com>
2025-01-19 14:27:06 +0100

    s

---
+++ CsAlgoPractice/Leetcode/L55_JumpGame/JumpGame.cs
@@ -0,0 +1,18 @@
+﻿namespace CsAlgoPractice.Leetcode.L55_JumpGame;
+
+public class JumpGame
+{
+    public bool CanJump(int[] nums)
+    {
+        int dist = nums[0];
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (i>dist)
+            {
+                return false;
+            }
+            dist = Math.Max(dist, nums[i]+i);
+        }
+        return true;
+    }
+}
