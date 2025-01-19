414604306916f3519337017cd04fbe0c35d07589
unknown <k25650727@gmail.com>
2025-01-19 14:53:19 +0100

    s

---
+++ CsAlgoPractice/Leetcode/L53_MaximumSubarray/MaximumSubarray.cs
@@ -0,0 +1,20 @@
+﻿namespace CsAlgoPractice.Leetcode.L53_MaximumSubarray;
+
+public class MaximumSubarray
+{
+    public int MaxSubArray(int[] nums)
+    {
+        int sum = nums[0], max = nums[0];
+        for (int i = 1; i < nums.Length; i++)
+        {
+            if (sum < 0)
+            {
+                sum = 0;
+            }
+            sum += nums[i];
+            max = Math.Max(max, sum);
+        }
+
+        return max;   
+    }
+}
