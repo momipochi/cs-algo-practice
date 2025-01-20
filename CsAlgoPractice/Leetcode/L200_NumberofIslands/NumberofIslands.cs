3935b49e761f211ec556bc270efd7867f8eb1f86
unknown <k25650727@gmail.com>
2025-01-20 15:05:25 +0100

    s

---
+++ CsAlgoPractice/Leetcode/L200_NumberofIslands/NumberofIslands.cs
@@ -0,0 +1,35 @@
+﻿namespace CsAlgoPractice.Leetcode.L200_NumberofIslands;
+
+public class NumberofIslands
+{
+    
+    public int NumIslands(char[][] grid)
+    {
+        int res = 0;
+        void Helper(int x, int y)
+        {
+            if (x < 0 || x >= grid.Length || y < 0 || y >= grid[0].Length || grid[x][y] == '0')
+            {
+                return;
+            }
+            grid[x][y] = '0';
+            Helper(x , y - 1);
+            Helper(x - 1, y );
+            Helper(x + 1, y );
+            Helper(x , y+1 );
+        }
+
+        for (int i = 0; i < grid.Length; i++)
+        {
+            for (int j = 0; j < grid[i].Length; j++)
+            {
+                if (grid[i][j] == '1')
+                {
+                    res++;
+                    Helper(i,j);
+                }
+            }
+        }
+        return res;
+    }
+}
