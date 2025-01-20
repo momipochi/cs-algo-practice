2811acafb7317e4a8a648c68d821492d09a0c19c
unknown <k25650727@gmail.com>
2025-01-20 14:57:28 +0100

    s

---
+++ CsAlgoPractice/Leetcode/L189_RotateArray/RotateArray.cs
@@ -0,0 +1,22 @@
+﻿namespace CsAlgoPractice.Leetcode.L189_RotateArray;
+
+public class RotateArray
+{
+    public void Rotate(int[] nums, int k) {
+        k %= nums.Length;
+        Helper(nums, 0, nums.Length-1);
+        Helper(nums, 0, k-1);
+        Helper(nums, k, nums.Length-1);
+    }
+
+    private void Helper(int[] nums, int start, int end)
+    {
+        while (start < end)
+        {
+            (nums[start], nums[end]) = (nums[end], nums[start]);
+            start++;
+            end--;
+        }
+        
+    }
+}
