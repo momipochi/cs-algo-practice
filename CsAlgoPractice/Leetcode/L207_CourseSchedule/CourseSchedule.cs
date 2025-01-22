3747afe7eb9a0e6870bfc97cf4e885030f322e92
unknown <k25650727@gmail.com>
2025-01-22 10:21:04 +0100

    s

---
+++ CsAlgoPractice/Leetcode/L207_CourseSchedule/CourseSchedule.cs
@@ -0,0 +1,36 @@
+﻿namespace CsAlgoPractice.Leetcode.L207_CourseSchedule;
+
+public class CourseSchedule
+{
+    public bool CanFinish(int numCourses, int[][] prerequisites)
+    {
+        int[] track = new int[numCourses];
+        for (int i = 0; i < prerequisites.Length; i++)
+        {
+            track[prerequisites[i][1]]++;
+        }
+        bool[] visisted = new bool[prerequisites.Length];
+        bool found = true;
+        while (found)
+        {
+            found = false;
+            for (int i = 0; i < prerequisites.Length; i++)
+            {
+                if (!visisted[i] && track[prerequisites[i][0]] == 0)
+                {
+                    visisted[i] = true;
+                    track[prerequisites[i][1]]--;
+                    found = true;
+                }
+            }
+        }
+        for (int i = 0; i < track.Length; i++)
+        {
+            if (track[i] != 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
