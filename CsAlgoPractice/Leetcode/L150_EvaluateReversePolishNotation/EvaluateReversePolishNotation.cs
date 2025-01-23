062c27f4b54d9949c73bd5402c0735d01f40c41e
unknown <k25650727@gmail.com>
2025-01-23 14:30:13 +0100

    commit

---
+++ CsAlgoPractice/Leetcode/L150_EvaluateReversePolishNotation/EvaluateReversePolishNotation.cs
@@ -0,0 +1,41 @@
+﻿namespace CsAlgoPractice.Leetcode.L150_EvaluateReversePolishNotation;
+
+public class EvaluateReversePolishNotation
+{
+    public int EvalRPN(string[] tokens)
+    {
+        Dictionary<string, Func<int,int,int>> strategy = new Dictionary<string, Func<int,int,int>>
+        {
+            ["+"] = (x, y) => x + y,
+            ["-"] = (x, y) => x - y,
+            ["*"] = (x, y) => x * y,
+            ["/"] = (x, y) => x / y
+        };
+
+        Stack<int> stk = new Stack<int>();
+        foreach (var token in tokens)
+        {
+            if (int.TryParse(token,out var v))
+            {
+                stk.Push(v);    
+            }
+            else
+            {
+                if (strategy.ContainsKey(token))
+                {
+                    int b = stk.Pop(), a = stk.Pop();
+                    stk.Push(strategy[token](a,b));
+                }
+                else
+                {
+                    throw new ArgumentException();
+                }    
+            }
+            
+            
+        }
+        
+        return stk.Pop();
+    }
+
+}
---
+++ CsAlgoPractice/Leetcode/L86_PartitionList/PartitionList.cs
@@ -0,0 +1,32 @@
+﻿using CsAlgoPractice.Models;
+
+namespace CsAlgoPractice.Leetcode.L86_PartitionList;
+
+public class PartitionList
+{
+    public ListNode Partition(ListNode head, int x)
+    {
+        var left = new ListNode();
+        var lHead = left;
+        var right = new ListNode();
+        var rHead = right;
+        var tmp = head;
+        while (tmp != null)
+        {
+            if (tmp.val < x)
+            {
+                left.next = tmp;
+                left = left.next;
+            }
+            else
+            {
+                right.next = tmp;
+                right = right.next;
+            }
+            tmp = tmp.next;
+        }
+        right.next = null;
+        left.next = rHead.next;
+        return lHead.next;
+    }
+}
