4772074c6439cb5cdae08a33867657e794fc4fbd
unknown <k25650727@gmail.com>
2025-01-23 14:40:14 +0100

    commit

---
+++ CsAlgoPractice/Leetcode/L121_BestTimeStock/BestTimeStock.cs
@@ -0,0 +1,20 @@
+﻿namespace CsAlgoPractice.Leetcode.L121_BestTimeStock;
+
+public class BestTimeStock
+{
+    public int MaxProfit(int[] prices)
+    {
+        int max = 0;
+        int min = prices[0];
+        for (int i = 1; i < prices.Length; i++)
+        {
+            if (min > prices[i])
+            {
+                min = prices[i];
+            }
+
+            max = Math.Max(max, prices[i] - min);
+        }
+        return max;
+    }
+}
