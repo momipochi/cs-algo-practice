f4be4cd201c48cae9adff46d06bdabf41f9640aa
unknown <k25650727@gmail.com>
2025-01-23 09:21:59 +0100

    commit

---
+++ CsAlgoPractice/Leetcode/L77_Combinations/Combinations.cs
@@ -0,0 +1,27 @@
+﻿namespace CsAlgoPractice.Leetcode.L77_Combinations;
+
+public class Combinations
+{
+    public IList<IList<int>> Combine(int n, int k)
+    {
+        IList<IList<int>> res = new List<IList<int>>();
+        Search(1,n,k,new List<int>(),res);
+        return res;
+    }
+
+    private void Search(int index, int n, int k, List<int> comb, IList<IList<int>> res)
+    {
+        if (k == 0)
+        {
+            res.Add(new List<int>(comb));
+            return;
+        }
+
+        for (int i = index; i <= n; i++)
+        {
+            comb.Add(i);
+            Search(i+1,n,k-1,comb,res);
+            comb.RemoveAt(comb.Count-1);
+        }
+    }
+}
