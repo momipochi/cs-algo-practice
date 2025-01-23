223f1995b5e7f297d92c5949f2e34c4097f2a1d9
unknown <k25650727@gmail.com>
2025-01-23 13:48:30 +0100

    commit

---
+++ CsAlgoPractice/Leetcode/L92_ReversedLinkedListII/ReversedLinkedListII.cs
@@ -0,0 +1,28 @@
+﻿using CsAlgoPractice.Models;
+
+namespace CsAlgoPractice.Leetcode.L92_ReversedLinkedListII;
+
+public class ReversedLinkedListII
+{
+    public ListNode ReverseBetween(ListNode head, int left, int right)
+    {
+        var res = new ListNode();
+        res.next = head;
+        var prev = res.next;
+        for (int i = 1; i < left-1; i++)
+        {
+            prev = prev.next;
+        }
+
+        var curr = prev.next;
+        for (int i = left; i < right; i++)
+        {
+            var tmp = prev.next;
+            curr.next = tmp.next;
+            tmp.next = prev.next;
+            prev.next = tmp;
+
+        }
+        return res.next;
+    }
+}
